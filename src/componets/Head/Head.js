04d2d7746225c53f6a9aea5e1810426c967ddb1b
unknown <mahsuma.rezai8@gmail.com>
2024-02-09 03:32:10 -0800

    Work on head style

--- src/componets/Head/Head.js
+++ src/componets/Head/Head.js
@@ -1,16 +1,21 @@
 import classes from './Head.module.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import headimg from '../img/head.png'
 const Head = (props) => {
     return (
         <div className={classes.head}>
-            <ul>
-                <li><Link to='/' className={classes.link}>Home</Link></li>
-                <li><Link to='/about' className={classes.link}>About</Link></li>
-                <li><Link to='/product' className={classes.link}>Product</Link></li>
-                <li><Link to='/fashion' className={classes.link}>Fahsion</Link></li>
-                <li><Link to='/new' className={classes.link}>News</Link></li>
-                <li><Link to='/contact' className={classes.link}>Contact Us</Link></li>
+            <ul className="flex flex-row justify-center gap-10 pt-12">
+                <li><Link to='/' className="font-bold text-xl list-none no-underline">Home</Link></li>
+                <li><Link to='/about' className="font-bold text-xl list-none no-underline">About</Link></li>
+                <li><Link to='/product' className="font-bold text-xl list-none no-underline ">Product</Link></li>
+                <li><Link to='/fashion' className="font-bold text-xl list-none no-underline">Fahsion</Link></li>
+                <li><Link to='/new' className="font-bold text-xl list-none no-underline">News</Link></li>
+                <li><Link to='/contact' className="font-bold text-xl list-none no-underline">Contact Us</Link></li>
             </ul>
+            <div className={classes.logo}>
+                <img src={headimg} alt="name" className="h-12"/>
+                <h1 className="text-red-700 font-bold text-3xl">CodeCollage</h1>
+            </div>
 
 
         </div>
--- src/componets/Pages/Home.js
+++ src/componets/Pages/Home.js
@@ -4,6 +4,7 @@ const Home=(peops)=>{
     return (
         <Fragment>
             <h1 className="text-red-500">heool</h1>
+            <h5>jjjjjjj</h5>
         </Fragment>
     )
 }
