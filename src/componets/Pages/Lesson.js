7855a15e3a57c63c44d79ec4614276d2dc028388
unknown <mahsuma.rezai8@gmail.com>
2024-02-18 08:23:59 -0800

    work on card section

--- src/componets/Pages/Lesson.js
+++ src/componets/Pages/Lesson.js
@@ -1,39 +1,73 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import classes from './Lesson.module.css';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Fragment } from "react";
-import React from "react";
-import classes from './Lesson.module.css'
-import Slider from "react-slick";
 
-const Lesson =(props)=>{
-    var settings = {
+
+const Lesson = () => {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
         style: {
             marginTop: '20px',
             marginBottom: '50px',
         },
-      };
+    };
+
     return (
-        <Fragment>
-            
-            <Slider {...settings}  className={classes.card} >
-            <div className={classes.box}>
-                <h3>1</h3>
+        <Slider {...settings} className= {classes.card}>
+            <div className=" flex flex-row justift-center items-center rounded-lg shadow-md lg:max-w-sm shadow-xl 
+             shadow-gray-100 mb-2   ">
+                <div className={classes.bcakimg}>
+
+                </div>
+                <div className="p-4">
+                    <h4 className="text-xl font-semibold tracking-tight text-blue-600 text-right">
+                        علی قوچانی
+                      </h4>
+                    <p className="mb-2 leading-normal text-right">
+                        مدرس PHP
+                     </p>
+
+                </div>
             </div>
-            <div className={classes.box}>
-                <h3>2</h3>
+            <div className=" rounded-lg shadow-md lg:max-w-sm shadow-xl  shadow-gray-100 mb-2 ">
+                <div className={classes.bcakimgtwo}>
+
+
+                </div>
+                <div className="p-4">
+                    <h4 className="text-xl font-semibold tracking-tight text-blue-600 text-right">
+                        علی قوچانی
+                      </h4>
+                    <p className="mb-2 leading-normal text-right">
+                        مدرس PHP
+                     </p>
+
+                </div>
             </div>
-            <div className={classes.box}>
-                <h3>3</h3>
+            <div className=" rounded-lg shadow-md lg:max-w-sm shadow-xl  shadow-gray-100 mb-2 ">
+                <div  className={classes.bcakimgthree}>
+
+                </div>
+                <div className="p-4">
+                    <h4 className="text-xl font-semibold tracking-tight text-blue-600 text-right">
+                        علی قوچانی
+                      </h4>
+                    <p className="mb-2 leading-normal text-right">
+                        مدرس PHP
+                     </p>
+
+                </div>
             </div>
-            
-    </Slider>
-        </Fragment>
-    )
-}
+
+        </Slider>
+    );
+};
+
 export default Lesson;
